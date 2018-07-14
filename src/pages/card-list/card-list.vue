4fc6db1aacce315e581c7b6366a9cf443c0131ae
panranran <ran@jkweixin.com>
2018-07-14 18:08:23 +0800

    优化合并

--- src/pages/card-list/card-list.vue
+++ src/pages/card-list/card-list.vue
@@ -97,7 +97,9 @@
           ext
         }
         let account = item.employee.im_account
+        console.log(option, account)
         webimHandler.onSendCustomMsg(option, account).then(() => {
+          console.log(999999)
           wx.switchTab({url: '/pages/card/card'})
         })
       },
